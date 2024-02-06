5e357361c521b34039cb5e68e048d029788b1f4d
Rohit-2206 <112386100+Rohit-2206@users.noreply.github.com>
2024-02-06 16:50:01 +0530

    Update MainModule.java

--- JavaCodingChallenge/src/com/hexaware/main/MainModule.java
+++ JavaCodingChallenge/src/com/hexaware/main/MainModule.java
@@ -6,6 +6,7 @@ import com.hexaware.controller.HospitalServiceImpl;
 import com.hexaware.entity.Appointment;
 import com.hexaware.entity.Doctor;
 import com.hexaware.entity.Patient;
+import com.hexaware.exception.PatientNumberNotFoundException;
 
 public class MainModule {
 
@@ -17,14 +18,13 @@ public class MainModule {
 		Appointment app = new Appointment();
 		Patient pt = new Patient();
 		Doctor dr = new Doctor();
-		
 		System.out.println("Enter what you want to do");
-		System.out.println("1.Get Appointment ID\n"
-				+ "2.Get Appointments for Patient\n"
-				+ "3.Get Appointmemts for Doctor\n"
+		System.out.println("1.Get Appointment by ID\n"
+				+ "2.Get Appointments by PatientID\n"
+				+ "3.Get Appointmemts by DoctorID\n"
 				+ "4.Schedule Appointment\n"
-				+ "5.Cancel Appointment\n"
-				+ "6.Return Back to Main Menu"
+				+ "5.Update Appointment\n"
+				+ "6.Cancel Appointment\n"
 				+ "7.Log Out\n");
 		int choice = read.nextInt();
 		
@@ -33,7 +33,11 @@ public class MainModule {
 			hsi.getAppointmentById(app.getAppointmentId());
 			break;
 		case 2:
-			hsi.getAppointmentsForPatient(pt.getPatientId());
+			try {
+				hsi.getAppointmentsForPatient(pt.getPatientId());
+			} catch (PatientNumberNotFoundException e) {
+				System.out.println(e.getMessage());
+			}
 			break;
 		case 3:
 			hsi.getAppointmentsForDoctor(dr.getDoctorId());
@@ -42,10 +46,11 @@ public class MainModule {
 			hsi.scheduleAppointment(app);
 			break;
 		case 5:
-			hsi.cancelAppointment(app.getAppointmentId());
+			hsi.updateAppoinment(app);
 			break;
 		case 6:
-			return;
+			hsi.cancelAppointment(app.getAppointmentId());
+			break;
 		case 7:
 			System.out.println("Thank You");
 			System.exit(0);
